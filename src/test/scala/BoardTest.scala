3f4217f726c67bc15cd3438c364394892b54c314
dan-mi-sun <dan.mi.sun@gmail.com>
2015-03-05 14:47:55 +0000

    comment possible tests to cover Board rules / constraints

--- src/test/scala/BoardTest.scala
+++ src/test/scala/BoardTest.scala
@@ -4,11 +4,17 @@ import org.scalatest.{Matchers, FunSpec}
 
 class BoardTest extends FunSpec with Matchers {
   
+  //possible tests:
+  //create a board that has a row or column full and make sure an exception is thrown if the player tries to place
+  //somewhere that will overflow.
+  //
+  //Define winning and make sure Game does correct thing for a win
+  //
+  //Make sure that moves are legal
+  
   describe("A Game") {
     it("can allow two Humans to play a game of Connect Four") {
-      val game = new Game(p1, p2);
-      game.setGUI(new GUI(game, Board.NUM_COLS, Board.NUM_ROWS))
-      game.runGame()
+     ???
       
     }
     
