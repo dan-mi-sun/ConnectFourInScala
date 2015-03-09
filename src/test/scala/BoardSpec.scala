30503466f1c0fe2e68f25448d62501f1c9cfa568
dan-mi-sun <dan.mi.sun@gmail.com>
2015-03-09 11:58:38 +0000

    fix name spacing for pending board spec

--- src/test/scala/BoardSpec.scala
+++ src/test/scala/BoardSpec.scala
@@ -1,7 +1,16 @@
-package com.danielsan.connectfour
 
-import org.scalatest.{Matchers, FunSpec}
+import org.scalatest.{FunSpec, Matchers}
 
 class BoardSpec extends FunSpec with Matchers {
-???
+  describe("A Game") {
+    it("should allow two Humans to play a game of Connect Four") (pending)
+//      val p1 = new Human(RED);
+//      val p2 = new Human(YELLOW);
+//      val game = new Game(p1, p2)
+//      game.setGUI(new GUI(game, Board.NUM_COLS, Board.NUM_ROWS))
+//      game.runGame()
+
+
+
+  }
 }
